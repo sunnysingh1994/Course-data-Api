f4bd4938a6081b67e9b5f42e69f61c668f786334
sunnysingh1994 <sunnysingh1994.ss@gmail.com>
2020-04-20 21:16:00 +0530

    final API

---
+++ src/main/java/io/springboot/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,6 @@
+package io.springboot.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String> {
+}
---
+++ src/main/java/io/springboot/springbootstarter/topic/TopicService.java
@@ -0,0 +1,37 @@
+package io.springboot.springbootstarter.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public List<Topic> getTopics() {
+         List<Topic> topics = new ArrayList<>();
+         topicRepository.findAll()
+                 .forEach(topics::add);
+                return topics;
+    }
+
+    public Topic getTopic(String id){
+       return topicRepository.findById(id).orElse(null);
+    }
+
+    public void addTopics(Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void updateTopics(Topic topic, String id) {
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topicRepository.deleteById(id);
+    }
+}
